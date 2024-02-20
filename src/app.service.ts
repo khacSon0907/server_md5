080620752b4f5d92203a1ea756fcda2554c7e72e
khacSon0907 <creatine09072004@gmail.com>
2024-02-19 19:19:29 -0800

    onakay

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return ' update prismaclient nhé anh ba ';
+    return ' A3 đang tu not love ';
   }
 }
