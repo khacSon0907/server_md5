641c033082dc754d373c19bba2becb8984c45fde
khacSon0907 <creatine09072004@gmail.com>
2024-02-19 19:58:37 -0800

    oolll

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return ' Lên Lên not that bai ';
+    return ' Lên Lên not that bai kk ';
   }
 }
